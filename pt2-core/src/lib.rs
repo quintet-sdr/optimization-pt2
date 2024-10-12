92043e3a86a54538ee947121655c78962d82e2a4
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-12 19:02:30 +0300

    .

--- pt2-core/src/lib.rs
+++ pt2-core/src/lib.rs
@@ -1,1 +1,20 @@
+use thiserror::Error;
 
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("method is not applicable")]
+    NotApplicable,
+    #[error("problem has no solution")]
+    NoSolution,
+}
+
+pub struct Solution {
+    /// A vector of the decision variables.
+    _x: Vec<f64>,
+    /// The maximum value of the objective function.
+    _z: f64,
+}
+
+pub fn interior_point() -> Result<Solution, Error> {
+    todo!()
+}
