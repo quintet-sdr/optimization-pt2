b98a150ef8a5cf96857175368f540c42d8bcf49e
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-27 00:25:51 +0300

    .

--- crates/pt2-core/src/lib.rs
+++ crates/pt2-core/src/lib.rs
@@ -1,44 +1,41 @@
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
-
 use na::{DMatrix, DVector};
 
 mod error;
 
-#[derive(Debug)]
 pub struct Solution {
     // /// A vector of the decision variables.
     // x: Vec<f64>,
     // /// The maximum value of the objective function.
     // z: f64,
-    x: Weak<RefCell<DVector<f64>>>,
+    x: DVector<f64>,
     objective_fn: DMatrix<f64>,
 }
 
 pub fn run() {
+    const EPS: usize = 3;
     let solution = solve(
         vec![2., 2., 4., 3.],
         vec![vec![2., -2., 8., 0.], vec![-6., -1., 0., -1.]],
         vec![-2., 3., 0., 0.],
         0.5,
-        3,
+        EPS,
     )
     .unwrap();
 
     for (i, iteration) in solution.enumerate().map(|(i, it)| (i + 1, it)) {
         let it = iteration.unwrap();
-        println!("{i}: {it:?}");
+        println!("Iteration {i}");
+        println!("x:{:.EPS$}", it.x);
+        println!("fn:{:.EPS$}", it.objective_fn);
     }
 }
 
 pub struct Ipa {
-    x: Rc<RefCell<DVector<f64>>>,
+    x: DVector<f64>,
     a: DMatrix<f64>,
     c: DVector<f64>,
     alpha: f64,
-    eps: u8,
+    eps: usize,
     done: bool,
 }
 
@@ -51,7 +48,7 @@ impl Iterator for Ipa {
             return None;
         }
 
-        let d = DMatrix::from_diagonal(&self.x.borrow());
+        let d = DMatrix::from_diagonal(&self.x);
 
         let aa = &self.a * &d;
         let cc = &d * &self.c;
@@ -72,16 +69,16 @@ impl Iterator for Ipa {
         let y = DVector::from_element(self.c.nrows(), 1.0) + (self.alpha / nu) * cp;
 
         let yy = d * y;
-        let norm = (&yy - &*self.x.borrow()).norm();
-        *self.x.borrow_mut() = yy;
+        let norm = (&yy - &self.x).norm();
+        self.x = yy;
 
-        if norm < 0.1_f64.powi(self.eps.into()) {
+        if norm < 0.1_f64.powi(self.eps as i32) {
             self.done = true;
         }
 
         Some(Ok(Solution {
-            x: Rc::downgrade(&self.x),
-            objective_fn: &self.c * self.x.borrow().transpose(),
+            x: self.x.clone_owned(),
+            objective_fn: &self.c * self.x.transpose(),
         }))
     }
 }
@@ -91,14 +88,14 @@ pub fn solve(
     a: Vec<Vec<f64>>,
     c: Vec<f64>,
     alpha: f64,
-    eps: u8,
+    eps: usize,
 ) -> Result<Ipa, error::NoSolution> {
     // if !todo!() {
     //     return Err(NoSolutionError);
     // }
 
     Ok(Ipa {
-        x: Rc::new(RefCell::new(DVector::from_vec(x))),
+        x: DVector::from_vec(x),
         a: DMatrix::from_row_iterator(a.len(), a.first().unwrap().len(), a.into_iter().flatten()),
         c: DVector::from_vec(c),
         alpha,
@@ -107,7 +104,7 @@ pub fn solve(
     })
 }
 
-// pub fn main(c: Vec<f64>, a: Vec<Vec<f64>>, initial_point: Vec<f64>, b: Vec<f64>, eps: u8) {
+// pub fn main(c: Vec<f64>, a: Vec<Vec<f64>>, initial_point: Vec<f64>, b: Vec<f64>, eps: usize) {
 //     assert_eq!(a.len(), b.len());
 //     a.iter().for_each(|row| assert_eq!(row.len(), c.len()));
 //     todo!();
