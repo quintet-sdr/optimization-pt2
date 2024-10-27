0c9396bdc10f0ee3139f487920e9ce0b5f1b8172
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-27 18:52:17 +0300

    .

--- crates/pt2-cli/src/tests.rs
+++ crates/pt2-cli/src/tests.rs
@@ -5,7 +5,11 @@ pub struct Lpp<'a> {
     pub rhs_numbers: &'a [f64],
 }
 
-pub fn generate_1<'a>() -> Lpp<'a> {
+pub fn all<'a>() -> &'a [fn() -> Lpp<'a>] {
+    &[lab_6_problem_2]
+}
+
+pub fn lab_6_problem_2<'a>() -> Lpp<'a> {
     Lpp {
         objective_function: vec![9., 10., 16.],
         constraints: &[&[18., 15., 12.], &[6., 4., 8.], &[5., 3., 3.]],
