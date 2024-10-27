3cf6da083dd78ebedbd4dac70185d6e6576ed482
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-27 17:21:48 +0300

    .

--- crates/pt2-core/src/lib.rs
+++ crates/pt2-core/src/lib.rs
@@ -6,6 +6,7 @@ use crate::interfaces::InteriorPoint;
 mod algorithm;
 mod interfaces;
 
+#[allow(clippy::many_single_char_names)]
 pub fn interior_point(
     c: Vec<f64>,
     a: Vec<Vec<f64>>,
@@ -21,21 +22,16 @@ pub fn interior_point(
         return Err(NotApplicableError);
     }
 
-    let x = DVector::from_vec(initial_point);
-
-    let big_a = {
-        let a = a.into_iter().flatten();
-        let mut big_a = DMatrix::from_row_iterator(n, m, a).resize_horizontally(n + m, 0.0);
-        big_a.view_mut((0, n), (n, n)).fill_with_identity();
-        big_a
-    };
-    let c = DVector::from_vec(c).resize_vertically(n + m, 0.0);
-
     Ok(InteriorPoint {
         done: false,
-        x,
-        big_a,
-        c,
+        x: DVector::from_vec(initial_point),
+        big_a: {
+            let a = a.into_iter().flatten();
+            let mut big_a = DMatrix::from_row_iterator(n, m, a).resize_horizontally(n + m, 0.0);
+            big_a.view_mut((0, n), (n, n)).fill_with_identity();
+            big_a
+        },
+        c: DVector::from_vec(c).resize_vertically(n + m, 0.0),
         alpha,
         eps: 0.1_f64.powi(<i32>::try_from(eps).map_err(|_| NotApplicableError)? + 1),
     })
