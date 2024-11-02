ceeb4e0250324c5d6d9c2852724ccfed3e2e1d11
Asqar Arslanov <asqarslanov@gmail.com>
2024-11-02 21:06:23 +0300

    .

--- crates/pt2-cli/src/config.rs
+++ crates/pt2-cli/src/config.rs
@@ -1,4 +1,6 @@
-use std::{fs::File, io::BufReader};
+use std::fmt::{self, Display, Formatter};
+use std::fs::File;
+use std::io::BufReader;
 
 use color_eyre::Result;
 use serde::Deserialize;
@@ -8,6 +10,7 @@ use pt2_core::Constraints;
 #[derive(Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Test {
+    pub name: Box<str>,
     pub objective_function: Vec<f64>,
     pub constraints: Constraints,
     pub initial_point: Vec<f64>,
@@ -15,9 +18,19 @@ pub struct Test {
     pub eps: usize,
 }
 
+impl Display for Test {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
 pub fn read_tests() -> Result<Vec<Test>> {
     let tests_file = BufReader::new(File::open("tests.json")?);
     Ok(serde_json::from_reader(tests_file)?)
+}
+
+fn name_default() -> Box<str> {
+    Box::from("Unnamed")
 }
 
 const fn eps_default() -> usize {
