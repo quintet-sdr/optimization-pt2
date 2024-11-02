d3d8b74eea85e31b19d19cc81e9fe3a58218b071
Asqar Arslanov <asqarslanov@gmail.com>
2024-11-02 16:50:37 +0300

    .

--- crates/pt2-core/src/lib.rs
+++ crates/pt2-core/src/lib.rs
@@ -78,8 +78,7 @@ fn build_big_a(constraints: &[(&[f64], Sign, f64)]) -> DMatrix<f64> {
     let no_slack_rows = constraints
         .iter()
         .enumerate()
-        .filter(|(_, (_, sign, _))| matches!(sign, Sign::Eq))
-        .map(|(i, _)| i);
+        .filter_map(|(i, (_, sign, _))| matches!(sign, Sign::Eq).then_some(i));
 
     let no_slack_columns = no_slack_rows.map(|i| m + i).collect::<Box<[_]>>();
 
