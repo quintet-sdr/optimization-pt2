3b3b92908434e9341f0275c5325d0c0b3edb89d5
Asqar Arslanov <asqarslanov@gmail.com>
2024-11-02 23:20:26 +0300

    .

--- crates/pt2-cli/src/cli.rs
+++ crates/pt2-cli/src/cli.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 use color_eyre::{eyre::Context, Result};
-use crossterm::terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 
 use crate::config::{self, Test};
 
@@ -16,18 +16,10 @@ enum Next {
     Break,
 }
 
-fn clear() -> io::Result<()> {
-    crossterm::execute!(io::stdout(), Clear(ClearType::All))
-}
-
 fn prompt(tests: Vec<Test>) -> Result<Next> {
-    const ALPHA_1: f64 = 0.5;
-    const ALPHA_2: f64 = 0.9;
-
     for _ in 0..crossterm::terminal::size()?.1 {
         println!()
     }
-    clear()?;
 
     let Some(test) = inquire::Select::new("Select a test:", tests)
         .with_vim_mode(true)
@@ -36,53 +28,52 @@ fn prompt(tests: Vec<Test>) -> Result<Next> {
         return Ok(Next::Break);
     };
 
-    for alpha in [ALPHA_1, ALPHA_2] {
-        let Ok((lpp, iterations)) = pt2_core::interior_point(
-            test.objective_function.clone(),
-            &test.constraints,
-            test.initial_point.clone(),
-            test.eps,
-            alpha,
-        ) else {
-            println!("The method is not applicable.");
-            break;
-        };
+    let Some(alpha) = inquire::Select::new("Select alpha:", vec![0.5, 0.9])
+        .with_vim_mode(true)
+        .prompt_skippable()?
+    else {
+        return Ok(Next::Break);
+    };
 
-        println!("Alpha: {alpha}");
+    let Ok((lpp, iterations)) = pt2_core::interior_point(
+        test.objective_function.clone(),
+        &test.constraints,
+        test.initial_point.clone(),
+        test.eps,
+        alpha,
+    ) else {
+        println!("The method is not applicable.");
+        return Ok(Next::Continue);
+    };
 
-        println!("Epsilon: {} ({:.eps$})", test.eps, lpp.eps, eps = test.eps);
+    println!("Epsilon: {} ({:.eps$})", test.eps, lpp.eps, eps = test.eps);
 
-        println!(
-            "Objective function: {:.eps$?}",
-            lpp.c.iter().collect::<Box<[_]>>(),
-            eps = test.eps,
-        );
-        println!(
-            "Initial point: {:.eps$?}",
-            lpp.x.iter().collect::<Box<[_]>>(),
-            eps = test.eps,
-        );
-        println!("Constraints:{:.eps$}", lpp.big_a, eps = test.eps);
+    println!(
+        "Objective function: {:.eps$?}",
+        lpp.c.iter().collect::<Box<[_]>>(),
+        eps = test.eps,
+    );
+    println!(
+        "Initial point: {:.eps$?}",
+        lpp.x.iter().collect::<Box<[_]>>(),
+        eps = test.eps,
+    );
+    println!("Constraints:{:.eps$}", lpp.big_a, eps = test.eps);
 
-        let last = iterations.last().unwrap();
+    let last = iterations.last().unwrap();
 
-        let Ok(result) = last else {
-            println!("The problem doesn't have a solution.");
-            println!();
-            continue;
-        };
+    let Ok(result) = last else {
+        println!("The problem doesn't have a solution.");
+        return Ok(Next::Continue);
+    };
 
-        println!("Result:");
-        println!("Maximum: {:.eps$}", result.max, eps = test.eps);
-        println!(
-            "Decision variables: {:.eps$?}",
-            result.decision_variables.iter().collect::<Box<[_]>>(),
-            eps = test.eps
-        );
-        println!();
-        println!("{}", "-".repeat(16));
-        println!();
-    }
+    println!("Result:");
+    println!("Maximum: {:.eps$}", result.max, eps = test.eps);
+    println!(
+        "Decision variables: {:.eps$?}",
+        result.decision_variables.iter().collect::<Box<[_]>>(),
+        eps = test.eps
+    );
 
     let Some(next) = inquire::Confirm::new("Next test?").prompt_skippable()? else {
         return Ok(Next::Break);
